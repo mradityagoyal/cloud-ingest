d11681f4a52d393282eb548d12e588d538d44ebe
Alan502 <alanrmorales@gmail.com>
2017-10-26 15:23:14 -0700

    Updates the gen_proto.sh script to generate javascript.

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
 # This script handles the compilation of protocol buffers into different
-# languages.
+# languages. This script should be run from the root of the repository.
+pbjs_rel_path="./webconsole/frontend/node_modules/protobufjs/bin/pbjs"
+pbts_rel_path="./webconsole/frontend/node_modules/protobufjs/bin/pbts"
 protoc --python_out=webconsole/backend/ proto/tasks.proto
+if ! [ -e "$pbjs_rel_path" ]
+then
+  echo "The pbjs binary was not found. Did you install the web frontend dependencies?"
+else
+  $pbjs_rel_path -t static-module -w commonjs -o tasks.js ./proto/tasks.proto
+  $pbts_rel_path -o ./webconsole/frontend/src/app/proto/tasks.d.ts ./webconsole/frontend/src/app/proto/tasks.js
+fi
