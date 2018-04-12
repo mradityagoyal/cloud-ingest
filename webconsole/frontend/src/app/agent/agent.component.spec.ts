41c946c438d30345726b800f049a327945401088
Tamas Zsebe <tamasz@google.com>
2018-04-12 16:56:52 -0700

    Fixing frontend tests

--- webconsole/frontend/src/app/agent/agent.component.spec.ts
+++ webconsole/frontend/src/app/agent/agent.component.spec.ts
@@ -1,19 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from '../util/common.test-util';
+import { AngularMaterialImporterModule } from '../angular-material-importer/angular-material-importer.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AgentComponent } from './agent.component';
+
+let activatedRouteStub: ActivatedRouteStub;
 
 describe('AgentComponent', () => {
   let component: AgentComponent;
   let fixture: ComponentFixture<AgentComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AgentComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    activatedRouteStub = new ActivatedRouteStub();
+    TestBed.configureTestingModule({
+      declarations: [
+        AgentComponent
+      ],
+      imports: [
+        AngularMaterialImporterModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {provide: ActivatedRoute, useValue: activatedRouteStub}
+      ]
+    }).compileComponents();
     fixture = TestBed.createComponent(AgentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
