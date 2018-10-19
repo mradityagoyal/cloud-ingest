caa32ef3de1c9b18374874f4b3047343f841548d
Mahmoud Bassiouny <mbassiouny@google.com>
2018-10-18 23:42:24 -0700

    Introduce ControlHandler to handle agent control messages.
    * Initially to update the jobrun BW map of the workprocessor.
    * It can be extended to do more than that.

---
+++ agent/control.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2018 Google Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package agent
+
+import (
+	"context"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/golang/glog"
+	"github.com/golang/protobuf/proto"
+
+	controlpb "github.com/GoogleCloudPlatform/cloud-ingest/proto/control_go_proto"
+)
+
+// ControlHandler is responsible for receiving control messages pushed by the backend service.
+type ControlHandler struct {
+	sub        *pubsub.Subscription
+	lastUpdate time.Time
+}
+
+// NewControlHandler creates an instance of ControlHandler.
+func NewControlHandler(sub *pubsub.Subscription) *ControlHandler {
+	return &ControlHandler{sub, time.Now()}
+}
+
+// HandleControlMessages starts handling control messages sent by the service. This
+// is blocking function, it will only return in case of non-retriable errors.
+// TODO(b/117972265): This method should detect control messages absence, and act accordingly.
+func (ch *ControlHandler) HandleControlMessages(ctx context.Context) error {
+	// Set the max outstanding messages to 1, so there is only one go routine processing
+	// the messages.
+	ch.sub.ReceiveSettings.MaxOutstandingMessages = 1
+	return ch.sub.Receive(ctx, ch.processMessage)
+}
+
+func (ch *ControlHandler) processMessage(ctx context.Context, msg *pubsub.Message) {
+	if ch.sub != nil {
+		defer msg.Ack()
+	}
+	if msg.PublishTime.Before(ch.lastUpdate) {
+		// Ignore stale messages.
+		glog.Errorf("Ignore stale message: %v, publish time: %v", string(msg.Data), msg.PublishTime)
+		return
+	}
+
+	var controlMsg controlpb.Control
+	if err := proto.Unmarshal(msg.Data, &controlMsg); err != nil {
+		glog.Errorf("error decoding msg %s with error %v.", string(msg.Data), err)
+		// Non-recoverable error. Will Ack the message to avoid delivering again.
+		return
+	}
+	glog.Infof("Handling controlMsg: %v", controlMsg)
+	jobrunsBW := make(map[string]int64)
+	for _, jobBW := range controlMsg.JobRunsBandwidths {
+		jobrunsBW[jobBW.JobrunRelRsrcName] = jobBW.Bandwidth
+	}
+	UpdateJobRunsBW(jobrunsBW)
+	ch.lastUpdate = msg.PublishTime
+}
---
+++ agent/control_test.go
@@ -0,0 +1,122 @@
+/*
+Copyright 2018 Google Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package agent
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
+
+	controlpb "github.com/GoogleCloudPlatform/cloud-ingest/proto/control_go_proto"
+)
+
+func marshalControlMessage(t *testing.T, msg *controlpb.Control) []byte {
+	data, err := proto.Marshal(msg)
+	if err != nil {
+		t.Fatalf("marshal control message: %v got err: %v", msg, err)
+	}
+	return data
+}
+
+func TestProcessMessage(t *testing.T) {
+	ctx := context.Background()
+	ch := NewControlHandler(nil)
+	now := time.Now()
+
+	tests := []struct {
+		desc string
+		msg  []byte
+		ts   time.Time
+		want map[string]int64
+	}{
+		{
+			desc: "first message, with no active jobs",
+			msg:  marshalControlMessage(t, &controlpb.Control{}),
+			ts:   now,
+			want: map[string]int64{},
+		},
+		{
+			desc: "outdated message, should not change the previous assignment",
+			msg: marshalControlMessage(t, &controlpb.Control{
+				JobRunsBandwidths: []*controlpb.JobRunBandwidth{
+					&controlpb.JobRunBandwidth{
+						JobrunRelRsrcName: "job-1",
+						Bandwidth:         20,
+					},
+				},
+			}),
+			ts:   now.Add(-10 * time.Second),
+			want: map[string]int64{},
+		},
+		{
+			desc: "some active job runs",
+			msg: marshalControlMessage(t, &controlpb.Control{
+				JobRunsBandwidths: []*controlpb.JobRunBandwidth{
+					&controlpb.JobRunBandwidth{
+						JobrunRelRsrcName: "job-1",
+						Bandwidth:         20,
+					},
+					&controlpb.JobRunBandwidth{
+						JobrunRelRsrcName: "job-2",
+						Bandwidth:         10,
+					},
+				},
+			}),
+			ts:   now.Add(10 * time.Second),
+			want: map[string]int64{"job-1": int64(20), "job-2": int64(10)},
+		},
+		{
+			desc: "no active job runs",
+			msg:  marshalControlMessage(t, &controlpb.Control{}),
+			ts:   now.Add(20 * time.Second),
+			want: map[string]int64{},
+		},
+		{
+			desc: "one active job runs",
+			msg: marshalControlMessage(t, &controlpb.Control{
+				JobRunsBandwidths: []*controlpb.JobRunBandwidth{
+					&controlpb.JobRunBandwidth{
+						JobrunRelRsrcName: "job-3",
+						Bandwidth:         30,
+					},
+				},
+			}),
+			ts:   now.Add(30 * time.Second),
+			want: map[string]int64{"job-3": int64(30)},
+		},
+		{
+			desc: "invalid message data bytes",
+			msg:  []byte("Invalid message"),
+			ts:   now.Add(40 * time.Second),
+			want: map[string]int64{"job-3": int64(30)},
+		},
+	}
+
+	for _, tc := range tests {
+		msg := &pubsub.Message{
+			Data:        tc.msg,
+			PublishTime: tc.ts,
+		}
+		ch.processMessage(ctx, msg)
+		if !cmp.Equal(activeJobRuns, tc.want) {
+			t.Errorf("processMessage(%q) set active job run to: %v, want: %v", tc.desc, activeJobRuns, tc.want)
+		}
+	}
+}
