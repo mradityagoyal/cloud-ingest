1db584f87ca0fb17869453aa9da6158c44498ab9
Ye Liu <yeliu@google.com>
2019-04-30 13:25:43 -0700

    Change control.proto to match the message in DCP

--- proto/gen_proto.sh
+++ proto/gen_proto.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script generates the Go protocol buffers for the protos in this dir.
-protoc --go_out=task_go_proto/ task.proto
-protoc --go_out=pulse_go_proto/ pulse.proto
-protoc --go_out=control_go_proto/ control.proto
-protoc --go_out=listfile_go_proto/ listfile.proto
+protoc --go_out=$(go env GOPATH)/src/ task.proto
+protoc --go_out=$(go env GOPATH)/src/ pulse.proto
+protoc --go_out=$(go env GOPATH)/src/ control.proto
+protoc --go_out=$(go env GOPATH)/src/ listfile.proto
