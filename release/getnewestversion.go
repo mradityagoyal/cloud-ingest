5c1f11fde8461f966d982e413225f7753903c3af
Vincent Chen <vinchen@google.com>
2019-08-14 11:26:45 -0700

    Adding automation ability to build script.

---
+++ release/getnewestversion.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path"
+	"strings"
+
+
+	"github.com/GoogleCloudPlatform/cloud-ingest/release/changelog"
+)
+
+func main() {
+  // Generate json bytes for the current changelog file
+  out, err := changelog.ParseChangelogFile(
+    path.Join(changelog.GoPath(), changelog.RepoPath, "CHANGELOG.md"))
+  if err != nil {
+    log.Fatalf("Failed to parse changelog and create json representation. Error: %v", err)
+  }
+
+  versions, err := changelog.ParseChangelogVersions(out)
+  if err != nil {
+    log.Fatalf("Failed to parse changelog versions. Error: %v", err)
+  }
+
+  err = changelog.ValidateChangelogVersions(versions)
+  if err != nil {
+    log.Fatalf("Changelog validation failed. Error: %v", err)
+  }
+
+  // Handle the unreleased case
+  if versions[0].Version == "" {
+      versions = versions[1:]
+  }
+
+  // Newest version in changelog in standard agent version format
+  fmt.Printf("v%s\n", strings.Trim(versions[0].Version, "{}"))
+}
--- release/release-helpers.sh
+++ release/release-helpers.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# GIT Commit ID (SHA1 Hash) regex
+GIT_COMMIT_REGEX=^[0-9a-f]{40}$
+
+# GCS_STAGING_PREFIX is the GCS location for builds
+GCS_STAGING_PREFIX=gs://cloud-ingest-rapid/agent
+
 # GCS_VERSIONS_PREFIX is the GCS 'folder' in which we archive all builds of the
 # agent, by version.
 GCS_VERSIONS_PREFIX=gs://cloud-ingest/agent/versions
