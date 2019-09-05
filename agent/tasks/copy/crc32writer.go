e8de02200604ffc92fdfe8791eb6727dac2b0465
Aditya Goyal <adityagoyal@google.com>
2019-09-05 10:57:42 -0400

    add crc32writer

---
+++ agent/tasks/copy/crc32writer.go
@@ -0,0 +1,33 @@
+package copy
+
+import (
+	"hash/crc32"
+	"io"
+)
+
+//TODO:(adityagoyal) check with @thobrla about reusing this table. 
+//var (
+//	CRC32CTable = crc32.MakeTable(crc32.Castagnoli)
+//)
+
+// CRC32UpdatingReader is an io.Reader that wraps another io.Reader and a
+// starting crc32c. This reader updates the crc32c as bytes are read.
+type CRC32UpdatingWriter struct {
+	writer io.Writer
+	curCRC *uint32
+}
+
+// NewCRC32UpdatingReader returns a CRC32UpdatingReader. 'currentCRC' is the
+// starting crc32 value for the reader, and will be updated as bytes are read.
+func NewCRC32UpdatingWriter(w io.Writer, currentCRC *uint32) io.Writer {
+	return &CRC32UpdatingWriter{writer: w, curCRC: currentCRC}
+}
+
+// Read implements the io.Reader interface.
+func (cr *CRC32UpdatingWriter) Write(buf []byte) (n int, err error) {
+	if n, err = cr.writer.Write(buf); err != nil {
+		return 0, err
+	}
+	*cr.curCRC = crc32.Update(*cr.curCRC, CRC32CTable, buf[:n])
+	return n, nil
+}
